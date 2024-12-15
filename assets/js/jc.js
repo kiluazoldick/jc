61e39db6a9f3500de73f00ebca3b8d22ee2efc7f
TheSolver0 <lucfotso0@gmail.com>
2024-12-15 16:04:29 +0100

    ajout du comment jouer et des ampoules de solutions

--- assets/js/jc.js
+++ assets/js/jc.js
@@ -1,4 +1,9 @@
 // var c = Math.ceil(Math.random()*15);
+if(!localStorage.getItem('count'))
+{
+   localStorage.setItem('count',3)
+}
+
 var code = document.getElementById("code");
 var c = parseInt(code.value);
 var textCode = document.getElementById("texteCode");
@@ -39,9 +44,20 @@ function verif(texte) {
          icon: 'success',
          confirmButtonText: 'Cool'
       });
+      
       lev++;
+      if(lev > 10)
+      {
+         lev = 1;
+         Swal.fire({
+            title: 'Felicitations 🎉🎉🎉!',
+            text: "Vous avez terminé la premiere partie du jeu!!!",
+            icon: 'success',
+            confirmButtonText: 'Cool'
+         });
+      }
       console.log(lev);
-      setTimeout(function () { window.location.href = '/' + lev }, 1000);
+      setTimeout(function () { window.location.href = '/' + lev }, 2000);
 
       return true;
    }
@@ -57,13 +73,39 @@ function verif(texte) {
    }
 
 }
-function revealSolution(e) {
-   e.preventDefault();
-   document.getElementById(
-      "solutionText"
-   ).textContent = `Réponse correcte : ${texte}`;
-   document.getElementById("solutionBox").style.display = "block";
+let count = localStorage.getItem('count');
+console.log('count',count);
+let sol = document.getElementById("solutionButton");
+for(i = 0; i < count; i++)
+{
+   sol.innerHTML += '<i class="fa-solid fa-lightbulb amp" style="color: #38f8b5" ></i>';
 }
+for(i = 0; i < 3-count; i++)
+{
+   sol.innerHTML += '<i class="fa-solid fa-lightbulb amp" style="color:#b7b4b4 " ></i>';
+}
+function revealSolution(e) {
+   
+   e.preventDefault();
+   if(count > 0)
+   {
+      document.getElementById(
+         "solutionText"
+      ).textContent = `Réponse correcte : ${texte}`;
+      document.getElementById("solutionBox").style.display = "block";
+
+      let amp = document.getElementsByClassName("amp");
+    
+      amp[count-1 ].style.color='#b7b4b4';
+
+      count -= 1; 
+      localStorage.setItem('count', count)
+      console.log('count',count);
+
+   }
+   
+}
+
 document
   .getElementById("solutionButton")
   .addEventListener("click", revealSolution);
